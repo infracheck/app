9fa2c62a5abe89b65b031653790c92b5616397b0
Martin Welcker <martinwelcker@gmx.de>
2020-07-01 11:33:01 +0200

    Add file logging

--- infracheck/__init__.py
+++ infracheck/__init__.py
@@ -6,14 +6,23 @@ from flask import Flask
 from Environment import Environment
 from infracheck.Persistence import Persistence
 
+# Log
 log = logging.getLogger()
-handler = logging.StreamHandler()
-formatter = logging.Formatter(
-    '%(levelname)-8s %(asctime)s    %(message)-38s  %(name)s', "%Y-%m-%d %H:%M:%S")
-handler.setFormatter(formatter)
-log.addHandler(handler)
-log.setLevel(Environment.LOG_LEVEL)
+formatter = logging.Formatter('%(levelname)-8s %(asctime)s    %(message)-38s  %(name)s', "%Y-%m-%d %H:%M:%S")
 
+# Stream Log
+stream_handler = logging.StreamHandler()
+stream_handler.setFormatter(formatter)
+log.setLevel(Environment.LOG_LEVEL)
+log.addHandler(stream_handler)
+
+# File Log
+file_handler = logging.FileHandler('debug.log')
+file_handler.setFormatter(formatter)
+file_handler.setLevel(logging.DEBUG)
+log.addHandler(file_handler)
+
+# Init Flask
 app = Flask(__name__)
 app.secret_key = 'secret'
 
