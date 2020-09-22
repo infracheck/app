4d858595f7c668c325073b56d1016d93bd80836f
Martin Welcker <martinwelcker@gmx.de>
2020-09-22 10:18:51 +0200

    Add description to the Persistence.py

--- backend/infracheck/Persistence.py
+++ backend/infracheck/Persistence.py
@@ -22,6 +22,10 @@ def singleton(class_):
 
 @singleton
 class Persistence:
+    """
+    This class deals as wrapper for the SQLAlchemy interface
+    Changes to the database model should be made here.
+    """
 
     def __init__(self) -> None:
         self.db = SQLAlchemy(app)
@@ -29,6 +33,10 @@ class Persistence:
 
     @dataclass
     class Result(db.Model):
+        """
+        ORM of the Result. It contains table definitions.
+        If the table does not exist, it will be created when the application starts.
+        """
         id: str = db.Column(db.String, primary_key=True)
         name: str = db.Column(db.String(80), nullable=False)
         description: str = db.Column(db.String(120), nullable=False)
@@ -41,6 +49,11 @@ class Persistence:
         date: datetime = db.Column(db.DateTime, server_default=func.now())
 
     def add_result(self, result: ITestResult):
+        """
+        Adds a rest result to the database
+        :param result:
+        :return:
+        """
         db.session.add(self.Result(
             id=result['id'],
             name=result['name'],
