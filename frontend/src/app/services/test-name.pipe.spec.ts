1c44d9d66314a275d6b8bd9a735992e57b3f39b6
Martin Welcker <martinwelcker@gmx.de>
2020-09-28 08:51:07 +0200

    Add icons

--- frontend/src/app/services/test-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {TestNamePipe} from './test-name.pipe';
-
-describe('TestNamePipe', () => {
-    it('create an instance', () => {
-        const pipe = new TestNamePipe();
-        expect(pipe).toBeTruthy();
-    });
-});
--- frontend/src/app/services/test-search.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {TestSearchPipe} from './test-search.pipe';
-
-describe('TestSearchPipe', () => {
-    it('create an instance', () => {
-        const pipe = new TestSearchPipe();
-        expect(pipe).toBeTruthy();
-    });
-});
